0906cb6186d64d0d6d9ee4f742750cf01c89d60c
isaac israel <email.iisrael@gmail.com>
2019-04-14 15:15:42 -0300

    Initialize API data fetch

--- src/services/vinService.ts
+++ src/services/vinService.ts
@@ -1,7 +1,15 @@
-// import { get } from "../utils/https"
+import { get } from "../utils/https"
 
 const invalidChars = new RegExp(/[IOQ]/, "g")
 const validateLenght = 17
+const apiURL = `https://vpic.nhtsa.dot.gov/api/vehicles/`
+const apiMap = {
+    make: "Make",
+    model: "Model",
+    year: "Model Year",
+    trim: "Trim",
+    vehicleType: "Vehicle Type"
+}
 
 export const filter = (vin: string) =>
     vin
@@ -12,27 +20,30 @@ export const filter = (vin: string) =>
 export const validate = (_vin: string): string =>
     _vin.length !== validateLenght ? `${validateLenght} chars expected` : ""
 
+const isResultEmpty = (result: VinCheckResponse): boolean => !result || !result.Results || result.Results.length == 0
+
 export const convert = (_res: VinCheckResponse): CarInfo => {
+    if (isResultEmpty(_res)) {
+        return null
+    }
+    // function for retravel the value
+    const retrievalValue = (field: string): string => {
+        const result = _res.Results.find(info => info.Variable === field)
+        return result ? result.Value : ""
+    }
     return {
-        make: "Make BMW",
-        model: "Model 528i",
-        year: 2011,
-        trim: "",
-        vehicleType: ""
+        make: retrievalValue(apiMap.make),
+        model: retrievalValue(apiMap.model),
+        year: parseInt(retrievalValue(apiMap.year), 10),
+        trim: retrievalValue(apiMap.trim),
+        vehicleType: retrievalValue(apiMap.vehicleType)
     }
 }
 
 export const apiCheck = async (_vin: string): Promise<CarInfo> => {
-    // Wait 2 seconds to simulate API call
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    return convert({
-        Results: [
-            {
-                Value: "",
-                ValueId: "",
-                Variable: "",
-                VariableId: 0
-            }
-        ]
-    })
+    try {
+        return convert((await get(`${apiURL}DecodeVin/${_vin}?format=json`)) as VinCheckResponse)
+    } catch (error) {
+        new Error(error)
+    }
 }
