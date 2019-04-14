d7a0809bb229d3114166c381a1d40cfec5327cbe
isaac israel <email.iisrael@gmail.com>
2019-04-14 08:08:13 -0300

    Fix test:unite

--- src/services/vinService.ts
+++ src/services/vinService.ts
@@ -1,7 +1,11 @@
 // import { get } from "../utils/https"
 
-// const invalidChars = new RegExp(/[I]/, "g")
-export const filter = (vin: string) => vin
+const invalidChars = new RegExp(/[IOQ]/, "g")
+export const filter = (vin: string) =>
+    vin
+        .toUpperCase()
+        .replace(invalidChars, "")
+        .substr(0, 17)
 
 export const validate = (_vin: string): string => null
 
